f2af9a57ace19ef679f7300c01db2f21ea0cee63
Sameer Segal <sameersegal@microsoft.com>
2025-12-11 18:45:43 +0530

    Added sunflight mode

--- config.js
+++ config.js
@@ -1,5 +1,2 @@
-// Configuration for Mela 2025 Entry System
-// For local testing with mock server, use: http://localhost:3000
-// For production, use your Google Apps Script Web App URL
+const API_URL = "https://script.google.com/macros/s/AKfycbzyuXrXt_szHYzEuvidF0ez0d3AlEViHJUjVwhe_H9SFISboF_E3A-3ZgtVaTyDN6MV/exec";
 
-const API_URL = "http://localhost:3000";
